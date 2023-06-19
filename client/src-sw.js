4f566085c32521dfa22ad2f1a0e10b9aa562496b
rmessett15 <rmessett15@gmail.com>
2023-06-19 15:33:40 -0500

    added webpack plugins and loaders, updated service worker, indexDB, and installed event listeners

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,6 +4,7 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { StaleWhileRevalidate } = require("workbox-strategies");
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -27,4 +28,16 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: "asset-cache",
+    plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,25 +13,45 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+    console.log('Update the database');
+
+  // Create a connection to the database database and version we want to use.
+  const textDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = textDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .put() method on the store and pass in the content.
+  const request = store.put({ id: 1, value: content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
+;
+
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   
   console.log("GET from the database");
-  console.error('getDb not implemented'); //???
 
-  const textDb = await openDB("text", 1);
+  const textDb = await openDB("jate", 1);
 
-  const tx = contactDb.transaction("text", "readonly");
+  const tx = textDb.transaction("jate", "readonly");
 
-  const store = tx.objectStore("text");
+  const store = tx.objectStore("jate");
 
-  const request = store.getAll();
+  const request = store.get(1);
 
   const result = await request;
   console.log("result.value", result);
-  return result;
+  return result?.value;
 };
 
 initdb();
